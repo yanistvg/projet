7aeddbc0edcd036c9fb4ad9c94e39a04435bdd80
yanis <yanisgenytv@gmail.com>
2018-12-24 17:02:28 +0100

    debut verif table

--- reset_base.php
+++ reset_base.php
@@ -1,2 +1,29 @@
 <?php
+$table = array();
+$table[0] = "users";
+$table[1] = "test";
+$serveurName = "localhost";
+$username = "root";
+$userpass = "";
+$conn = new mysqli($serveurName, $username, $userpass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+$etat = tableExist($conn, "users");
+echo $etat;
+
+function tableExist($database , $tableName)
+{
+    $b=false;
+    $reponse = $database->query('show tables');
+        while($data = $reponse->fetch())
+        {
+            if($data[0] == tableName )
+            {
+                $b = true;
+            }
+        }
+        return $b;
+    }
 ?>
